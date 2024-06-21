40ad958747a7ac8c707a1762744dbbba279ce353
Javier Amaya <Javier.amaya@itb.cat>
2024-06-21 11:55:32 +0200

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Chessboard)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Square)
+})
+basic.forever(function () {
+	
+})
